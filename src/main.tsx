174adb1e614ec52b33892476bf9d1979a93c127d
MuazzamAli777 <mozambhali521@gmail.com>
2026-06-27 16:10:25 -0700

    Updated portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-XXXXXXXXXX");
+ReactGA.send("pageview");
   createRoot(document.getElementById("root")!).render(<App />);
   
